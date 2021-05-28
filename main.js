80bad5c5bfb0d86b85aadbeb03bd872046164c20
r3surr3cti0n <matraattila2004@gmail.com>
2021-05-28 16:40:43 +0200

    Done.

--- main.js
+++ main.js
@@ -1,68 +1,74 @@
-chances = ["rock", "paper", "scissor"];
-localStorage.setItem("playerScore", 0);
-localStorage.setItem("computerScore", 0);
-let playerScore = localStorage.getItem("playerScore");
-let computerScore = localStorage.getItem("computerScore");
+import { chances, computerPlay } from "./computer.js";
 
-let computerPlay = () => {
-	return chances[Math.floor(chances.length * Math.random())];
-};
+let playerScore = 0;
+let computerScore = 0;
+
+let playerSelection;
+
+const subTitle = document.querySelector(".sub-title");
+const startText = document.querySelector(".start-text");
+// Score
+const compScoreOut = document.querySelector(".computer-score");
+const playerScoreOut = document.querySelector(".player-score");
+// Choice
+const compChoiceOut = document.querySelector(".computer-choice");
+const playerChoiceOut = document.querySelector(".player-choice");
+// Buttons
+const rockBtn = document.querySelector(".rock-btn");
+const paperBtn = document.querySelector(".paper-btn");
+const scissorBtn = document.querySelector(".scissor-btn");
+
+rockBtn.addEventListener("click", () => {
+	playerSelection = chances[0];
+	Game(computerPlay(), playerSelection);
+});
+paperBtn.addEventListener("click", () => {
+	playerSelection = chances[1];
+	Game(computerPlay(), playerSelection);
+});
+scissorBtn.addEventListener("click", () => {
+	playerSelection = chances[2];
+	Game(computerPlay(), playerSelection);
+});
 
 let Game = (computerSelection, playerSelection) => {
 	// Tie
 	if (computerSelection == playerSelection) {
-		console.log("It's a tie.");
+		subTitle.innerHTML = "Ez a kör döntetlen.";
 	}
 	// Player
 	if (computerSelection == chances[0] && playerSelection == chances[1]) {
-		console.log("Paper defeats rock. Player won.");
+		subTitle.innerHTML = "A papír legyőzi a követ. Játékosé a pont.";
 		playerScore++;
 	}
 	if (computerSelection == chances[1] && playerSelection == chances[2]) {
-		console.log("Scissor defeats paper. Player won.");
+		subTitle.innerHTML = "Az olló legyőzi a papírt. Játékosé a pont.";
 		playerScore++;
 	}
 	if (computerSelection == chances[2] && playerSelection == chances[0]) {
-		console.log("Rock defeats scissor. Player won.");
+		subTitle.innerHTML = "A kő legyőzi az ollót. Játékosé a pont.";
 		playerScore++;
 	}
 	// Computer
 	if (computerSelection == chances[1] && playerSelection == chances[0]) {
-		console.log("Paper defeats rock. Computer won.");
+		subTitle.innerHTML = "A papír legyőzi a követ. Számítógépé a pont.";
 		computerScore++;
 	}
 	if (computerSelection == chances[2] && playerSelection == chances[1]) {
-		console.log("Scissor defeats paper. Computer won.");
+		subTitle.innerHTML = "Az olló legyőzi a papírt. Számítógépé a pont.";
 		computerScore++;
 	}
 	if (computerSelection == chances[0] && playerSelection == chances[2]) {
-		console.log("Rock defeats scissor. Computer won.");
+		subTitle.innerHTML = "A kő legyőzi az ollót. Számítógépé a pont.";
 		computerScore++;
 	}
+
+	// Choice
+	compChoiceOut.innerHTML = `<img src="img/${computerSelection}.png" >`;
+	playerChoiceOut.innerHTML = `<img src="img/${playerSelection}.png" >`;
+	// Score
+	compScoreOut.innerHTML = computerScore;
+	playerScoreOut.innerHTML = playerScore;
+
+	startText.classList.add("hide");
 };
-
-// Repeat the game 10 times
-for (let i = 0; i < 10; i++) {
-	let playerChoice = prompt(`Round: ${i + 1} | Rock, paper or scissor?`);
-
-	let containsWord =
-		playerChoice.indexOf(chances[0]) &&
-		playerChoice.indexOf(chances[1]) &&
-		playerChoice.indexOf(chances[2]);
-	console.log(`Round: ${i + 1}. Contains word: ${containsWord}`);
-
-	if (
-		playerChoice !== undefined &&
-		playerChoice !== null &&
-		containsWord >= 0
-	) {
-		playerChoice = playerChoice.toLowerCase();
-		Game(computerPlay(), playerChoice);
-	} else {
-		i--;
-		alert("Choose between rock,paper or scissor.");
-	}
-}
-
-// Scores
-alert(`Computer score: ${computerScore} | Player score: ${playerScore}`);
